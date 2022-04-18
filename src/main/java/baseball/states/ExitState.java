fb815e6b0729b8f7437fcb36f2c144c13775ea29
Kyujin Kang <digiry@gmail.com>
2022-04-18 19:54:15 +0900

    feat(State): implement exit state

--- src/main/java/baseball/states/ExitState.java
+++ src/main/java/baseball/states/ExitState.java
@@ -15,21 +15,21 @@ public class ExitState implements State {
 
     @Override
     public void viewUpdate() {
-
+        // Nothing
     }
 
     @Override
     public String readInput() {
-        return null;
+        return gameController.getEmptyInputData();
     }
 
     @Override
     public void evaluatePlayerData(String input) {
-
+        gameController.evaluatePlayerData(input);
     }
 
     @Override
     public void nextState() {
-
+        // Nothing
     }
 }
