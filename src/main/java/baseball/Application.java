7f8007afdd5655ea67933a2bdd84b98ffc3674e3
Kyujin Kang <digiry@gmail.com>
2022-04-18 20:25:38 +0900

    feat(Application): implement main method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,11 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        DefensePlayer player = new DefensePlayer();
+        GameView view = new GameView();
+        GameController controller = new GameController(player, view);
+        GameBoard board = new GameBoard(controller);
+
+        board.run();
     }
 }
