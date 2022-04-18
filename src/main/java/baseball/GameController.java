657fa21d7e1fe81b989bef2fc4a3bab97cddcf3a
Kyujin Kang <digiry@gmail.com>
2022-04-18 17:45:09 +0900

    feat(GameController): add methods to forword call to player, view

---
+++ src/main/java/baseball/GameController.java
@@ -0,0 +1,55 @@
+package baseball;
+
+public class GameController {
+    private DefensePlayer defensePlayer;
+    private GameView gameView;
+
+    public GameController(DefensePlayer player, GameView view) {
+        defensePlayer = player;
+        gameView = view;
+    }
+
+    public void evaluatePlayerData(String data) {
+        defensePlayer.evaluateData(data);
+    }
+
+    public int getBallCount() {
+        return defensePlayer.getBallCount();
+    }
+
+    public int getStrikeCount() {
+        return defensePlayer.getStrikeCount();
+    }
+
+    public boolean isTripleStrike() {
+        return defensePlayer.isTripleStrike();
+    }
+
+    public void printInputRequestMessage() {
+        gameView.printInputRequestMessage();
+    }
+
+    public void printGameResult() {
+       gameView.printGameResult(defensePlayer.getBallCount(), defensePlayer.getStrikeCount());
+    }
+
+    public void printStageDoneMessage() {
+        gameView.printStageDoneMessage();
+    }
+
+    public void printMenuMessage() {
+        gameView.printMenuMessage();
+    }
+
+    public void printCloseMessage() {
+        gameView.printCloseMessage();
+    }
+
+    public String readNumbersInput() {
+        return gameView.readNumbersInput();
+    }
+
+    public String readMenuInput() {
+        return gameView.readMenuInput();
+    }
+}
